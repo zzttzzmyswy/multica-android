770d382e67d7340dc98d404cad51903b5ca3170b
Naiyuan Qing <145280634+NevilleQingNY@users.noreply.github.com>
2026-04-01 22:24:35 +0800

    Revert "feat: add global issue search"

--- apps/web/features/modals/store.ts
+++ apps/web/features/modals/store.ts
@@ -2,7 +2,7 @@
 
 import { create } from "zustand";
 
-type ModalType = "create-workspace" | "create-issue" | "search-issues" | null;
+type ModalType = "create-workspace" | "create-issue" | null;
 
 interface ModalStore {
   modal: ModalType;
